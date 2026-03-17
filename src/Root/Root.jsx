3ab1c35a546326a60cb0dba8e6a2b7edc85d2f2d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 17:25:31 +0600

    outltate implemetn

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import { Outlet } from 'react-router';
 
 const Root = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <div >
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
