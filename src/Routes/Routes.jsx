c55ce19ecaa928440f4ec182609f9d224d5ef10e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 17:29:45 +0600

    registaion and navbar update

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,3 +1,6 @@
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Registation from "../Components/Registation/Registation";
 import Root from "../Root/Root";
 
 import { createBrowserRouter } from "react-router";
@@ -5,7 +8,13 @@ import { createBrowserRouter } from "react-router";
 const router = createBrowserRouter([
     {
         path: '/',
-        Component: Root
+        Component: Root,
+        children: [
+            { index: true, Component: Home },
+            { path: 'registaion', Component: Registation },
+            { path: 'login', Component: Login },
+
+        ]
     }
 ])
 export default router
