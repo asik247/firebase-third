4d98058328ac0d976109e2f9aa9b7f2d1adaa192
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 17:41:14 +0600

    registain code start now

--- src/Components/Registation/Registation.jsx
+++ src/Components/Registation/Registation.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 const Registation = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+            <div className="hero-content flex-col">
+                <div className="text-center   w-[400px] mx-auto">
+                    <h1 className="text-5xl font-bold mb-4">Registation now!</h1>
+                   
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
-                        <fieldset className="fieldset">
-                            <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-                            <button className="btn btn-neutral mt-4">Login</button>
-                        </fieldset>
+                        {/* form code start here */}
+                        <form>
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" className="input" placeholder="Password" />
+                               
+                                <button className="btn btn-neutral mt-4">Registation</button>
+                            </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
