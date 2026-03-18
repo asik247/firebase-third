3e880c83287f2d10363d9d8c1e29bd99110626c1
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-18 11:00:15 +0600

    Sign Up implement

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
         <li><NavLink to={'/'}>Home</NavLink></li>
         <li><NavLink to={'/registaion'}>Registaion</NavLink></li>
         <li><NavLink to={'/login'}>Login</NavLink></li>
+        <li><NavLink to={'/signup'}>SignUP</NavLink></li>
     </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import Home from "../Components/Home/Home";
 import Login from "../Components/Login/Login";
 import Registation from "../Components/Registation/Registation";
+import SignUp from "../Components/SignUp/SignUp";
 import Root from "../Root/Root";
 
 import { createBrowserRouter } from "react-router";
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
             { index: true, Component: Home },
             { path: 'registaion', Component: Registation },
             { path: 'login', Component: Login },
+            { path: 'signup', Component: SignUp },
 
         ]
     }
