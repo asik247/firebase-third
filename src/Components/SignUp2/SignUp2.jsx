cf10f2bf613df48bc900c07ab3a1b50bc0d787a6
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-19 10:43:37 +0600

    signup second time practics

--- src/Components/SignUp2/SignUp2.jsx
+++ src/Components/SignUp2/SignUp2.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const SignUp2 = () => {
     return (
         <div>
-            
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col ">
+                    <div className="text-center ">
+                        <h1 className="text-5xl font-bold">SignUp2 now!</h1>
+
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="label">Email</label>
+                                <input type="email" className="input" placeholder="Email" />
+                                <label className="label">Password</label>
+                                <input type="password" className="input" placeholder="Password" />
+
+                                <button className="btn btn-neutral mt-4">SingUP2</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
