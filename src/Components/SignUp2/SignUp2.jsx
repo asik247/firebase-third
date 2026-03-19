350e17df30bca7f084477b041e9bb0dce0e9fa23
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-19 10:51:19 +0600

    hook implement and form value clg log

--- src/Components/SignUp2/SignUp2.jsx
+++ src/Components/SignUp2/SignUp2.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import useMyHook2 from '../../Hooks/useMyHook2';
 
 const SignUp2 = () => {
+    // Hook code start here;
+    const [emailValue, handleEmailChange] = useMyHook2('');
+    const [passwordValue, handlePasswordChange] = useMyHook2('');
+    const handleSubmitBtn = (e) => {
+        e.preventDefault();
+        console.log(emailValue, passwordValue);
+    }
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen">
@@ -11,14 +19,18 @@ const SignUp2 = () => {
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <div className="card-body">
-                            <fieldset className="fieldset">
-                                <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
-                                <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
+                            <form onSubmit={handleSubmitBtn}>
+                                <fieldset className="fieldset">
+                                    {/* Email input field */}
+                                    <label className="label">Email</label>
+                                    <input type="email" value={emailValue} onChange={handleEmailChange} className="input" placeholder="Email" />
+                                    {/* Password input field */}
+                                    <label className="label">Password</label>
+                                    <input type="password" value={passwordValue} onChange={handlePasswordChange} className="input" placeholder="Password" />
 
-                                <button className="btn btn-neutral mt-4">SingUP2</button>
-                            </fieldset>
+                                    <button className="btn btn-neutral mt-4">SingUP2</button>
+                                </fieldset>
+                            </form>
                         </div>
                     </div>
                 </div>
